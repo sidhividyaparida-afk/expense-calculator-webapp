2ccf266549da1c7cd0081b13a69aef3a205342b6
S <sidhividyaparida@gmail.com>
2026-07-23 18:57:51 +0530

    added .env

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,15 +1,19 @@
+import os
 from fastapi import FastAPI,HTTPException,Depends
 # from app.routes.expense import router
 from app.routes.route import routes
 from app.models import model
 from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = FastAPI(
     title="Expense Calculator API",
     version="1.0.0"
 )
 
-origins = ["http://localhost:5173"]
+origins = os.getenv("ORIGINS")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,

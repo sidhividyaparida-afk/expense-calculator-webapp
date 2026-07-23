20181429790c2932197c15ccbf92d63d245a1549
S <sidhividyaparida@gmail.com>
2026-07-23 12:07:11 +0530

    connecting FB

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,10 +2,20 @@ from fastapi import FastAPI,HTTPException,Depends
 # from app.routes.expense import router
 from app.routes.route import routes
 from app.models import model
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="Expense Calculator API",
     version="1.0.0"
+)
+
+origins = ["http://localhost:5173"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
 )
 
 app.include_router(routes)
